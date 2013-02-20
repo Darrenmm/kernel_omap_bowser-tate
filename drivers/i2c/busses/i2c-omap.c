1711a47966390a51c38c2debd3072d55ec39238e
Hashcode <hashcode0f@gmail.com>
2013-02-20 11:11:46 -0800

    [amazon] drivers/i2c/busses: added I2C_TIMEOUT_DEBUG logic

--- drivers/i2c/busses/i2c-omap.c
+++ drivers/i2c/busses/i2c-omap.c
@@ -42,6 +42,7 @@
 #include <linux/i2c-omap.h>
 #include <linux/pm_runtime.h>
 #include <linux/pm_qos_params.h>
+#include <plat/omap_device.h>
 
 /* I2C controller revisions */
 #define OMAP_I2C_REV_2			0x20
@@ -156,9 +157,22 @@ enum {
 #define OMAP_I2C_SYSTEST_SDA_I		(1 << 1)	/* SDA line sense in */
 #define OMAP_I2C_SYSTEST_SDA_O		(1 << 0)	/* SDA line drive out */
 
+/* OCP_SYSCONFIG bit definitions */
+#define SYSC_CLOCKACTIVITY_MASK         (0x3 << 8)
+#define SYSC_SIDLEMODE_MASK             (0x3 << 3)
+#define SYSC_ENAWAKEUP_MASK             (1 << 2)
+#define SYSC_SOFTRESET_MASK             (1 << 1)
+#define SYSC_AUTOIDLE_MASK              (1 << 0)
+
+#define SYSC_IDLEMODE_SMARTWKUP         0x3
+#define SYSC_CLOCKACTIVITY_FCLK         0x2
+
 /* Errata definitions */
 #define I2C_OMAP_ERRATA_I207		(1 << 0)
 #define I2C_OMAP3_1P153			(1 << 1)
+
+#define I2C_TIMEOUT_DEBUG
+#define I2C_TIMEOUT_DEBUG_SIZE           4
 
 struct omap_i2c_dev {
 	struct device		*dev;
@@ -189,9 +203,15 @@ struct omap_i2c_dev {
 	u16			scllstate;
 	u16			sclhstate;
 	u16			bufstate;
-	u16			syscstate;
+	u16                     syscstate;
 	u16			westate;
 	u16			errata;
+
+#ifdef  I2C_TIMEOUT_DEBUG
+        struct i2c_msg          last_msg[I2C_TIMEOUT_DEBUG_SIZE];
+        u8                      last_msg_idx;
+#endif
+
 };
 
 const static u8 reg_map[] = {
@@ -241,6 +261,38 @@ const static u8 omap4_reg_map[] = {
 	[OMAP_I2C_IRQENABLE_SET] = 0x2c,
 	[OMAP_I2C_IRQENABLE_CLR] = 0x30,
 };
+
+
+static inline void omap_i2c_log_msg(struct omap_i2c_dev *i2c_dev, struct i2c_msg *msg)
+{
+#ifdef  I2C_TIMEOUT_DEBUG
+   u8 id = i2c_dev->last_msg_idx;
+   memcpy(&i2c_dev->last_msg[id], msg, sizeof(*msg));
+   i2c_dev->last_msg_idx = (id+1) % I2C_TIMEOUT_DEBUG_SIZE;
+#endif
+}
+
+static inline void omap_i2c_log_msg_init(struct omap_i2c_dev *i2c_dev)
+{
+#ifdef  I2C_TIMEOUT_DEBUG
+   i2c_dev->last_msg_idx = 0;
+   memset(&i2c_dev->last_msg[0], 0, sizeof(struct i2c_msg) * I2C_TIMEOUT_DEBUG_SIZE);
+#endif
+}
+
+static inline void omap_i2c_log_msg_print(struct omap_i2c_dev *i2c_dev)
+{
+#ifdef  I2C_TIMEOUT_DEBUG
+   int i;
+   u8 id = i2c_dev->last_msg_idx;
+
+   for(i=0; i< I2C_TIMEOUT_DEBUG_SIZE; i++) {
+      dev_err(i2c_dev->dev, "last_msg[%d]: addr=0x%04x, len=%d, flags=0x%x\n",
+              id, i2c_dev->last_msg[id].addr, i2c_dev->last_msg[id].len, i2c_dev->last_msg[id].flags);
+      id = (id+1) % I2C_TIMEOUT_DEBUG_SIZE;
+   }
+#endif
+}
 
 static inline void omap_i2c_write_reg(struct omap_i2c_dev *i2c_dev,
 				      int reg, u16 val)
@@ -336,13 +388,58 @@ static void omap_i2c_idle(struct omap_i2c_dev *dev)
 	pm_runtime_put_sync(&pdev->dev);
 }
 
-static inline void omap_i2c_reset(struct omap_i2c_dev *dev)
+static int omap_i2c_reset(struct omap_i2c_dev *dev)
 {
-	if (!dev->device_reset)
-		return;
+	unsigned long timeout;
+	/* TTX-897: Fix Wi-Fi on/off causes system mysteriously reboots
+	 * TTX-3735: Battery capacity show 0% which against actual battery
+	 *		capacity while doing manual power on/off test
+	 * TTX-2798: Device stuck on boot screen after factory reset loop
+	 *		or reboot loop test or flashing loop test
+	 * The arch/arm/mach-omap2/i2c.c: omap_i2c_reset() function that
+	 * this function used to call is not restoring the OMAP_I2C_SYSC_REG
+	 * register after a soft reset of I2C module. As a result, the clocks
+	 * for the I2C module were getting cut even after we call
+	 * pm_runtime_get_sync().
+	 * So, we explicity soft reset the I2C module ourselves and restore
+	 * the SYSC register
+	 */
+	/* Disable I2C controller before soft reset */
+	omap_i2c_write_reg(dev, OMAP_I2C_CON_REG,
+		omap_i2c_read_reg(dev, OMAP_I2C_CON_REG) &
+			~(OMAP_I2C_CON_EN));
 
-	if (dev->device_reset(dev->dev) < 0)
-		dev_err(dev->dev, "reset failed\n");
+	omap_i2c_write_reg(dev, OMAP_I2C_SYSC_REG, SYSC_SOFTRESET_MASK);
+	/* According to TRM 23.1.4.3 HS I2C Software Reset we need to set
+	 * the EN bit before the before we start checking on reset done
+	 */
+	timeout = jiffies + OMAP_I2C_TIMEOUT;
+	omap_i2c_write_reg(dev, OMAP_I2C_CON_REG, OMAP_I2C_CON_EN);
+	while (!(omap_i2c_read_reg(dev, OMAP_I2C_SYSS_REG) &
+			SYSS_RESETDONE_MASK)) {
+		if (time_after(jiffies, timeout)) {
+			dev_warn(dev->dev, "timeout waiting "
+				"for controller reset\n");
+			return -ETIMEDOUT;
+		}
+		mdelay(1);
+	}
+
+	/* SYSC register is cleared by the reset; rewrite it */
+	if (dev->rev == OMAP_I2C_REV_ON_2430) {
+		omap_i2c_write_reg(dev, OMAP_I2C_SYSC_REG,
+					SYSC_AUTOIDLE_MASK);
+	} else if (dev->rev >= OMAP_I2C_REV_ON_3430) {
+		dev->syscstate = SYSC_AUTOIDLE_MASK;
+		dev->syscstate |= SYSC_ENAWAKEUP_MASK;
+		dev->syscstate |= (SYSC_IDLEMODE_SMARTWKUP <<
+					__ffs(SYSC_SIDLEMODE_MASK));
+		dev->syscstate |= (SYSC_CLOCKACTIVITY_FCLK <<
+					__ffs(SYSC_CLOCKACTIVITY_MASK));
+		omap_i2c_write_reg(dev, OMAP_I2C_SYSC_REG,
+					dev->syscstate);
+	}
+	return 0;
 }
 
 static int omap_i2c_init(struct omap_i2c_dev *dev)
@@ -352,6 +449,14 @@ static int omap_i2c_init(struct omap_i2c_dev *dev)
 	unsigned long fclk_rate = 12000000;
 	unsigned long internal_clk = 0;
 	struct clk *fclk;
+	int ret;
+
+	/* Soft reset the I2C module */
+	ret = omap_i2c_reset(dev);
+	if (ret) {
+		dev_err(dev->dev, "Unable to softreset I2C module\n");
+		return ret;
+	}
 
 	if (dev->rev >= OMAP_I2C_REV_ON_3430) {
 		/*
@@ -484,11 +589,11 @@ static int omap_i2c_init(struct omap_i2c_dev *dev)
 static int omap_i2c_wait_for_bb(struct omap_i2c_dev *dev)
 {
 	unsigned long timeout;
-
 	timeout = jiffies + OMAP_I2C_TIMEOUT;
 	while (omap_i2c_read_reg(dev, OMAP_I2C_STAT_REG) & OMAP_I2C_STAT_BB) {
 		if (time_after(jiffies, timeout)) {
 			dev_warn(dev->dev, "timeout waiting for bus ready\n");
+                        omap_i2c_log_msg_print(dev);
 			return -ETIMEDOUT;
 		}
 		msleep(1);
@@ -504,6 +609,8 @@ static int omap_i2c_bus_clear(struct omap_i2c_dev *dev)
 {
 	u16 w;
 
+	dev_info(dev->dev, "BUS BUSY: hit errata: i694\n");
+
 	/* Per the I2C specification, if we are stuck in a bus busy state
 	 * we can attempt a bus clear to try and recover the bus by sending
 	 * at least 9 clock pulses on SCL. Put the I2C in a test mode so it
@@ -516,7 +623,6 @@ static int omap_i2c_bus_clear(struct omap_i2c_dev *dev)
 	msleep(1);
 	omap_i2c_write_reg(dev, OMAP_I2C_SYSTEST_REG, w);
 	omap_i2c_write_reg(dev, OMAP_I2C_CON_REG, 0);
-	omap_i2c_reset(dev);
 	omap_i2c_init(dev);
 	return omap_i2c_wait_for_bb(dev);
 }
@@ -533,6 +639,8 @@ static int omap_i2c_xfer_msg(struct i2c_adapter *adap,
 
 	dev_dbg(dev->dev, "addr: 0x%04x, len: %d, flags: 0x%x, stop: %d\n",
 		msg->addr, msg->len, msg->flags, stop);
+
+        omap_i2c_log_msg(dev, msg);
 
 	if (msg->len == 0)
 		return -EINVAL;
@@ -600,7 +708,7 @@ static int omap_i2c_xfer_msg(struct i2c_adapter *adap,
 	dev->buf_len = 0;
 	if (r == 0) {
 		dev_err(dev->dev, "controller timed out\n");
-		omap_i2c_reset(dev);
+                omap_i2c_log_msg_print(dev);
 		omap_i2c_init(dev);
 		return -ETIMEDOUT;
 	}
@@ -610,7 +718,6 @@ static int omap_i2c_xfer_msg(struct i2c_adapter *adap,
 
 	/* We have an error */
 	if (dev->cmd_err & OMAP_I2C_STAT_AL) {
-		omap_i2c_reset(dev);
 		omap_i2c_init(dev);
 		return -EIO;
 	}
@@ -653,9 +760,8 @@ omap_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 		return r;
 
 	/* We have the bus, enable IRQ */
-	enable_irq(dev->irq);
-
 	omap_i2c_unidle(dev);
+	enable_irq(dev->irq);
 
 	r = omap_i2c_wait_for_bb(dev);
 	if (r < 0)
@@ -686,9 +792,10 @@ omap_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 
 	omap_i2c_wait_for_bb(dev);
 out:
+	disable_irq(dev->irq);
 	omap_i2c_idle(dev);
 	omap_i2c_hwspinlock_unlock(dev);
-	disable_irq(dev->irq);
+
 	return r;
 }
 
@@ -861,7 +968,8 @@ complete:
 			err |= OMAP_I2C_STAT_NACK;
 
 		if (stat & OMAP_I2C_STAT_AL) {
-			dev_err(dev->dev, "Arbitration lost\n");
+			dev_err(dev->dev, "Dump stack on Arbitration lost\n");
+			dump_stack();
 			err |= OMAP_I2C_STAT_AL;
 		}
 		/*
@@ -1023,6 +1131,8 @@ omap_i2c_probe(struct platform_device *pdev)
 		r = -ENOMEM;
 		goto err_release_region;
 	}
+
+        omap_i2c_log_msg_init(dev);
 
 	if (pdata) {
 		speed = pdata->clkrate;
